db20358efc7a465190a81a908d987326d0a6a8ad
Saikat Bose <Saikat.Bose@sapiens.com>
2022-09-01 22:39:49 +0530

    Initail Setup for frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const notes = require("./data/notes")
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server is runing in PORT ${PORT}`))
+
+app.get("/", (req, res) => {
+    res.send("API is running")
+})
+
+app.get("/api/notes", (req, res) => {
+    res.send(notes)
+})
+
+app.get("/api/notes/:id", (req, res) => {
+    const note = notes.find(n => n._id === req.params.id)
+    res.send(note)
+})
